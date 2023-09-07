94486ff7c05b6314e3484b43e84a17d1a8f875f3
Gabriel D Minzemalulu <90185518+Gabriel-Dee@users.noreply.github.com>
2023-09-07 14:09:53 +0300

    Add files via upload

---
+++ streamlit.py
@@ -0,0 +1,38 @@
+# one
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
+
+# two
+dataframe = np.random.randn(10, 20)
+st.dataframe(dataframe)
+
+# three
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+
+st.dataframe(dataframe.style.highlight_max(axis=0))
+
+# four
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+st.table(dataframe)
+
+# five
